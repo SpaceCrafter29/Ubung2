b338191aaf0bad93958d57fee9ba9fc422490932
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-10-24 21:04:17 +0200

    Added h3

---
+++ src/h3/H3_main.java
@@ -0,0 +1,24 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i = 140;
+        int j = 150;
+        int k = -10;
+        boolean ausdruck1 = i > j;
+        boolean ausdruck2 = i > 200;
+        boolean ausdruck3 = j > 100;
+        if (ausdruck1 && !(ausdruck2) && !(ausdruck3)) {
+            k = 1;
+        } else if (ausdruck1 && ausdruck2 && !(ausdruck3)) {
+            k = 2;
+        } else if (ausdruck1 && ausdruck2 && ausdruck3) {
+            k = 3;
+        } else if (!(ausdruck1) && !(ausdruck2) && !(ausdruck3)) {
+            k = 4;
+        }
+    }
+}
+
+
+
