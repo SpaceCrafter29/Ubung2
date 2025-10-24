e4916d6a19b76bca7b24b824d72cefebd4a0d009
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-10-24 19:01:46 +0200

    Changed >= to > in if h1

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -3,7 +3,7 @@ package h1;
 public class H1_main {
     public static void main(String[] args) {
         int z = 18;
-        if (z >= 10) {
+        if (z > 10) {
             z -= 10;
         }
         System.out.println(z);
